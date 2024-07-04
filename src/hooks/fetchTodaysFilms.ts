9db91474b7170c8f71e200a2b1b41c1c7d6629bb
ilyainavoid <thirdilno@gmail.com>
2024-07-04 18:06:08 +0700

    feat: add fetchTodaysFilms hook

---
+++ src/hooks/fetchTodaysFilms.ts
@@ -0,0 +1,26 @@
+import {toast} from "react-toastify";
+import {Film} from "../interfaces/film.ts";
+import {getTodaysFilms} from "../utils/api/cinema/today/todayRequest.ts";
+import {useEffect, useState} from "react";
+
+const useFetchTodaysFilms = () => {
+    const [filmList, setFilmList] = useState<Film[]>([]);
+
+    useEffect(() => {
+        const fetchTodaysFilms = async () => {
+            try {
+                const response = await getTodaysFilms();
+                const fetchedFilms: Film[] = response.data.films;
+                setFilmList(fetchedFilms);
+            } catch (error) {
+                toast('Произошла ошибка при получении списка фильмов!');
+            }
+        };
+
+        fetchTodaysFilms();
+    }, []);
+
+    return filmList;
+};
+
+export default useFetchTodaysFilms;
