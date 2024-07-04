eeacb41a66f6ae9bd816218f8c7cb484c9766515
ilyainavoid <thirdilno@gmail.com>
2024-07-04 20:12:12 +0700

    fix: add missed user ratings field

--- src/interfaces/film.ts
+++ src/interfaces/film.ts
@@ -1,5 +1,6 @@
 import {FilmPerson} from "./filmPerson.ts";
 import {Country} from "./country.ts";
+import {UserRatings} from "./userRatings.ts";
 
 export interface Film {
     id: string;
@@ -10,6 +11,7 @@ export interface Film {
     actors: FilmPerson[];
     directors: FilmPerson[];
     genres: string[];
+    userRatings: UserRatings;
     runtime: number;
     ageRating: 'G' | 'PG' | 'PG13' | 'R' | 'NC17';
     img: string;
