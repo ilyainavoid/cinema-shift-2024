e00d0fc8e5a6f1183d2961e4719450d72b10253b
ilyainavoid <thirdilno@gmail.com>
2024-07-05 03:11:32 +0700

    feat: add button with different types

---
+++ src/components/UI/Button/Button.tsx
@@ -0,0 +1,18 @@
+import {FC} from "react";
+import styles from './button.module.scss';
+interface ButtonProps {
+    className?: string;
+    type: 'Transparent' | 'Primary';
+    children: string;
+    onClick?: () => void;
+}
+
+const Button: FC<ButtonProps> = ({className, type, children, onClick}) => {
+    const classnames = `${styles.button} ${styles[`button${type}`]} ${className || ''}`.trim();
+
+    return (
+        <button onClick={onClick} className={classnames}>{children}</button>
+    )
+}
+
+export default Button;
