662198dcaf3f995ec7a021df10a1505f5f8dbdc8
ilyainavoid <thirdilno@gmail.com>
2024-07-05 03:12:39 +0700

    feat(ui): add fractional rating component

---
+++ src/components/UI/FractionalRating/FractionalRating.tsx
@@ -0,0 +1,26 @@
+import {FC} from 'react';
+import Rating from 'react-rating';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
+import styles from './fractionalrating.module.scss';
+
+interface FractionalRatingProps {
+    rating: number;
+}
+
+const FractionalRating: FC<FractionalRatingProps> = ({ rating }) => {
+    return (
+        <Rating
+            className={styles.rating}
+            initialRating={Math.round(rating * 10) / 20}
+            fractions={10}
+            readonly
+            start={0}
+            stop={5}
+            emptySymbol={<FontAwesomeIcon icon={fasStar} className={styles.starEmpty}/>}
+            fullSymbol={<FontAwesomeIcon icon={fasStar} className={styles.starFull} />}
+        />
+    );
+};
+
+export default FractionalRating;
